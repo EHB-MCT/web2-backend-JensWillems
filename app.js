97a8561f9b19a8e41e845ed7c03d4b1cab0bc8fd
JensWillems <71693119+JensWillems@users.noreply.github.com>
2021-12-08 14:32:22 +0100

    heroku part 3

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get('/api/markers', async (req, res) => {
         const coll = db.collection(process.env.COLL)
         const markers = await coll.find({}).toArray();
 
-        res.status(200).send(markers);
+        //res.status(200).send(markers);
         return res.status(200).json("Hello form markers endpoint!");
     } catch (err) {
         console.log('get', err);
