211aa861e2ae3df4bb4767abeded4876d81ca4c5
JensWillems <71693119+JensWillems@users.noreply.github.com>
2021-12-10 17:23:52 +0100

    register and login form POST WIP

---
+++ README.md
@@ -0,0 +1,11 @@
+1: 
+2: 
+3: Used the 3 coding along: Boardgame video's for the .env implanted: 
+
+https://www.youtube.com/watch?v=JAFhwfyYEb4
+https://www.youtube.com/watch?v=3Ykr6dZjXhE&feature=youtu.be&ab_channel=MikeDerycke
+
+https://www.youtube.com/watch?v=o7Mh1zOAjDY&ab_channel=MikeDerycke
+
+https://www.youtube.com/watch?v=o7Mh1zOAjDY&ab_channel=MikeDerycke
+
--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoClient = require('mongodb').MongoClient;
-const dbConfig = require('./dbConfig.json');
+
+
 require('dotenv').config();
 
 
@@ -20,7 +21,6 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(bodyParser.json());
 
-
 //  tells you the absolute path of the directory containing the currently executing file.
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
@@ -28,9 +28,9 @@ app.get('/', (req, res) => {
 
 app.get('/api/markers', async (req, res) => {
     try {
-        await client.connect()
-        const db = client.db(process.env.DV)
-        const coll = db.collection(process.env.COLL)
+        await client.connect();
+        const db = client.db(process.env.DV);
+        const coll = db.collection(process.env.COLL);
         const markers = await coll.find({}).toArray();
 
         res.status(200).send(markers);
@@ -49,6 +49,75 @@ app.get('/api/markers', async (req, res) => {
 
 })
 
+
+app.post('/api/register', async (req, res) => {
+    // console.log("hello");
+    try {
+        await client.connect();
+        console.log(req.body);
+        const db = client.db(process.env.DV);
+        const coll = db.collection(process.env.COLLUSER);
+
+        const existingUser = await coll.findOne({
+            userMail: req.body.userMail
+        });
+        console.log(existingUser);
+        if (!existingUser) {
+            await coll.insertOne({
+                userMail: req.body.userMail,
+                userPW: req.body.userPW
+            });
+            res.status(200).send("user succesfully made!");
+        } else {
+            res.status(409).send("user has been taken!");
+        }
+    } catch (err) {
+        console.log('get', err);
+        res.status(500).send({
+            err: 'Something went wrong. Try again later',
+            value: err
+        })
+    } finally {
+        await client.close();
+    }
+})
+
+
+app.post('/api/login', async (req, res) => {
+    // console.log("hello");
+    try {
+        await client.connect();
+        const db = client.db(process.env.DV);
+        const coll = db.collection(process.env.COLLUSER);
+
+        const user = await coll.findOne({
+            userMail: req.body.loginMail
+        });
+        console.log(user);
+        if (user) {
+            if (user.userPW === req.body.loginPW) {
+                res.status(200).send("user succesfully loged in");
+            } else {
+                res.status(401).send({
+                    message: "incorrect username or password!"
+                });
+            }
+
+        } else if (!user) {
+            res.status(401).send("incorrect username or password!");
+        }
+    } catch (err) {
+        console.log('get', err);
+        res.status(500).send({
+            err: 'Something went wrong. Try again later',
+            value: err
+        })
+    } finally {
+        await client.close();
+    }
+})
+
+
 app.listen(port, () => {
-    console.log(`API is running at http://localhost:${port}/api/markers`);
+    console.log(`API is running at http://localhost:${port}`);
 })
