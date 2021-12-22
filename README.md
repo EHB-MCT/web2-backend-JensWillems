8f9cc6e646e435e50e426c55a32ebd33f0e106f2
JensWillems <71693119+JensWillems@users.noreply.github.com>
2021-12-22 08:05:41 +0100

    added a small delete function and a put

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ https://www.youtube.com/watch?v=o7Mh1zOAjDY&ab_channel=MikeDerycke
 
 https://www.youtube.com/watch?v=o7Mh1zOAjDY&ab_channel=MikeDerycke
 
+
--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoClient = require('mongodb').MongoClient;
+const {
+    send
+} = require('process');
 
 
 require('dotenv').config();
@@ -26,6 +29,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
+
+
 app.get('/api/markers', async (req, res) => {
     try {
         await client.connect();
@@ -48,6 +53,72 @@ app.get('/api/markers', async (req, res) => {
     //console.log(markers)
 
 })
+
+app.post('/api/yourscore', async (req, res) => {
+    try {
+        await client.connect();
+        const db = client.db(process.env.DV);
+        const coll = db.collection(process.env.COLLUSER);
+
+        const user = await coll.findOne({
+            userMail: req.body.userMail
+        });
+        // console.log(user, req.body.userMail, req.body.score);
+        if (user) {
+            res.status(200).send({
+                score: user.score || "Don't have a score yet"
+            });
+        } else(
+            res.status(412).send("Something went wrong!")
+        )
+    } catch (err) {
+        res.status(500).send({
+            err: 'Something went wrong. Try again later',
+            value: err
+        })
+    } finally {
+        await client.close()
+    }
+});
+
+app.delete('/api/yourscore', async (req, res) => {
+    try {
+        await client.connect();
+        const db = client.db(process.env.DV);
+        const coll = db.collection(process.env.COLLUSER);
+
+        const user = await coll.findOne({
+            userMail: req.body.userMail
+        });
+        // console.log(user, req.body.userMail, req.body.score);
+
+
+        if (user) {
+            const query = {
+                score: 1
+            };
+            const deleteScore = await coll.updateOne({
+                userMail: req.body.userMail
+            }, {
+                $unset: query,
+            });
+            console.log(deleteScore);
+
+            res.status(200).send("Score succesfully deleted!");
+            console.log(user)
+        } else(
+            res.status(412).send("Something went wrong!")
+        )
+    } catch (err) {
+        res.status(500).send({
+            err: 'Something went wrong. Try again later',
+            value: err
+        })
+    } finally {
+        await client.close()
+    }
+});
+
 
 app.use('/api/user', require('./user.router')());
 
