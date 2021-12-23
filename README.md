eca62b8d9a6c79f6d1ff19f7de7f478a03af2994
JensWillems <71693119+JensWillems@users.noreply.github.com>
2021-12-23 10:54:37 +0100

    final touches for the backend
    cleaning up the code , fillinf in the readMe file

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-1: 
-2: 
-3: Used the 3 coding along: Boardgame video's for the .env implanted: 
-
+Java Script:
+Used the 3 coding along: Boardgame video's for the .env and Heroku implanted and use of the Routes :
 https://www.youtube.com/watch?v=JAFhwfyYEb4
 https://www.youtube.com/watch?v=3Ykr6dZjXhE&feature=youtu.be&ab_channel=MikeDerycke
-
 https://www.youtube.com/watch?v=o7Mh1zOAjDY&ab_channel=MikeDerycke
 
-https://www.youtube.com/watch?v=o7Mh1zOAjDY&ab_channel=MikeDerycke
+Used for the delete a particular field:
+https://docs.mongodb.com/manual/reference/operator/update/unset/
 
+extra help with front-ed Post request:
+https://stackoverflow.com/questions/6396101/pure-javascript-send-post-data-without-a-form
 
+extra help with findOne and fineOneAndUpdate:
+https://stackoverflow.com/questions/43401790/using-findone-and-findoneandupdate-with-http-request-mongoose
+
+css:
+used w3schools for flexbox:
+https://www.w3schools.com/css/css3_flexbox.asp 
--- app.js
+++ app.js
@@ -7,19 +7,15 @@ const {
     send
 } = require('process');
 
-
 require('dotenv').config();
-
 
 // Mongo Client
 const client = new mongoClient(process.env.FINAL_URL, {
     useNewUrlParser: true
 })
 
-
 const app = express();
 const port = process.env.PORT;
-
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -29,8 +25,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-
-
+// Route for all the markers for on the map
 app.get('/api/markers', async (req, res) => {
     try {
         await client.connect();
@@ -39,7 +34,6 @@ app.get('/api/markers', async (req, res) => {
         const markers = await coll.find({}).toArray();
 
         res.status(200).send(markers);
-        //return res.status(200).json("Hello form markers endpoint!");
     } catch (err) {
         console.log('get', err);
         res.status(500).send({
@@ -54,6 +48,114 @@ app.get('/api/markers', async (req, res) => {
 
 })
 
+// The user can register
+app.post('/api/register', async (req, res) => {
+    try {
+        await client.connect();
+        console.log(req.body);
+        const db = client.db(process.env.DV);
+        const coll = db.collection(process.env.COLLUSER);
+
+        const existingUser = await coll.findOne({
+            userMail: req.body.userMail
+        });
+        console.log(existingUser);
+        if (!existingUser) {
+            await coll.insertOne({
+                userMail: req.body.userMail,
+                userPW: req.body.userPW,
+                score: 0
+            });
+            res.status(200).send("user succesfully made!");
+        } else {
+            res.status(409).send("user has been taken!");
+        }
+    } catch (err) {
+        console.log('get', err);
+        res.status(500).send({
+            err: 'Something went wrong. Try again later',
+            value: err
+        })
+    } finally {
+        await client.close();
+    }
+});
+
+// User can login
+app.post('/api/login', async (req, res) => {
+    try {
+        await client.connect();
+        const db = client.db(process.env.DV);
+        const coll = db.collection(process.env.COLLUSER);
+
+        const user = await coll.findOne({
+            userMail: req.body.loginMail
+        });
+        console.log(user);
+        if (user) {
+            if (user.userPW === req.body.loginPW) {
+                res.status(200).send({
+                    user: user.userMail
+                });
+            } else {
+                res.status(401).send({
+                    message: "incorrect username or password!"
+                });
+            }
+
+        } else if (!user) {
+            res.status(401).send("incorrect username or password!");
+        }
+    } catch (err) {
+        console.log('get', err);
+        res.status(500).send({
+            err: 'Something went wrong. Try again later',
+            value: err
+        })
+    } finally {
+        await client.close();
+    }
+})
+
+// the users score will be saved on MongoDB and updated
+app.put('/api/updatescore', async (req, res) => {
+    try {
+        await client.connect();
+        const db = client.db(process.env.DV);
+        const coll = db.collection(process.env.COLLUSER);
+
+        const user = await coll.findOne({
+            userMail: req.body.userMail
+        });
+        console.log(user, req.body.userMail, req.body.score);
+
+        if (user) {
+            await coll.findOneAndUpdate({
+                userMail: req.body.userMail
+            }, {
+                $set: {
+                    score: req.body.score
+                },
+            });
+            res.status(200).send("score updated!")
+        } else(
+            res.status(412).send("Something went wrong!")
+        )
+
+
+    } catch (err) {
+        console.log('get', err);
+        res.status(500).send({
+            err: 'Something went wrong. Try again later',
+            value: err
+        })
+
+    } finally {
+        await client.close();
+    }
+})
+
+// The user can see its score with this route
 app.post('/api/yourscore', async (req, res) => {
     try {
         await client.connect();
@@ -63,7 +165,6 @@ app.post('/api/yourscore', async (req, res) => {
         const user = await coll.findOne({
             userMail: req.body.userMail
         });
-        // console.log(user, req.body.userMail, req.body.score);
         if (user) {
             res.status(200).send({
                 score: user.score || "Don't have a score yet"
@@ -81,6 +182,7 @@ app.post('/api/yourscore', async (req, res) => {
     }
 });
 
+// User can delete its score
 app.delete('/api/yourscore', async (req, res) => {
     try {
         await client.connect();
@@ -90,9 +192,6 @@ app.delete('/api/yourscore', async (req, res) => {
         const user = await coll.findOne({
             userMail: req.body.userMail
         });
-        // console.log(user, req.body.userMail, req.body.score);
-
-
         if (user) {
             const query = {
                 score: 1
@@ -119,10 +218,7 @@ app.delete('/api/yourscore', async (req, res) => {
     }
 });
 
-
-app.use('/api/user', require('./user.router')());
-
-
+// Main route
 app.listen(port, () => {
     console.log(`API is running at http://localhost:${port}`);
 })
